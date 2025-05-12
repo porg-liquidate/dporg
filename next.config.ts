35b810fc5813943a3ebdfba61b068d53671e4374
UnclePhil1 <philipnssien@gmail.com>
2025-05-12 20:16:47 +0100

    FIX: routing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // For static exports
+  // OR for SSR:
+  // trailingSlash: true,
 };
 
 export default nextConfig;
